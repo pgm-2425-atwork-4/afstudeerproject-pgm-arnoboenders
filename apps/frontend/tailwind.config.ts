2e5dac7b2236dc25bda0846de5165366044c6764
Arno-Boenders <arnoboenders@gmail.com>
2025-02-24 19:35:25 +0100

    Added navigation

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        primaryHover: "var(--primary-hover)",
       },
     },
   },
