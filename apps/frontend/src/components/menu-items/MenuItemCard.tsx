895eaeef30ae407c083e7fbb827cefd94cd8dd12
Arno-Boenders <arnoboenders@gmail.com>
2025-02-26 15:00:45 +0100

    Update for menuItemCard

--- apps/frontend/src/components/menu-items/MenuItemCard.tsx
+++ apps/frontend/src/components/menu-items/MenuItemCard.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 interface Item {
   name: string;
   image: string;
@@ -9,22 +7,12 @@ interface Item {
 
 export default function MenuItemCard({ item }: { item: Item }) {
   return (
-    <div className="flex items-center justify-between gap-4 rounded-xl p-4">
-      <div className="relative w-48 h-48">
-        <Image
-          src={item.image}
-          alt={item.name}
-          layout="fill"
-          className="shadow-xl rounded-xl object-cover"
-        />
-      </div>
-      <div className="w-4/6">
+    <div className="flex flex-col rounded-xl">
         <div className="flex justify-between items-center">
           <h3>{item.name}</h3>
           <p>{item.price}</p>
         </div>
         <p className="italic text-sm">{item.ingredients}</p>
-      </div>
     </div>
   );
 }
