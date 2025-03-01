bfedbbc4f818d938eeb994e6f500c721e3fa64dd
Arno-Boenders <arnoboenders@gmail.com>
2025-03-01 16:58:54 +0100

    Fix for layouts

--- apps/frontend/src/app/(dashboard)/layout.tsx
+++ apps/frontend/src/app/(dashboard)/layout.tsx
@@ -1,16 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "@/app/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Loos-backoffice",
@@ -22,13 +10,5 @@ export default function PrivateLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="nl">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  ); // No nav, no footer
+  return <> {children}</>; // No nav, no footer
 }
