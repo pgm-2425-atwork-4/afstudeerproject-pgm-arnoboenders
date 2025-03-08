3349d88d88dea8a164c28e7faa41eb9d09b46171
pgm-arnoboenders <arnoboen@student.arteveldehs.be>
2025-03-08 12:54:03 +0100

    Update backoffice styling

--- apps/frontend/src/app/(dashboard)/private/page.tsx
+++ apps/frontend/src/app/(dashboard)/private/page.tsx
@@ -3,7 +3,6 @@
 import { redirect } from "next/navigation";
 import EditMenu from "@/app/(dashboard)/private/components/EditMenu";
 import LogoutButton from "@/components/functional/button/LogoutButton";
-import UploadImage from "./components/UploadImage";
 import { useAuth } from "@/components/context/AuthProvider";
 
 export default function PrivatePage() {
@@ -15,14 +14,16 @@ export default function PrivatePage() {
   return (
     <div className="max-w-2xl mx-auto mt-10 p-4">
       <div className="flex justify-between items-center">
-        <h2>Welcome {user.email}</h2>
+        <h2>Welkom, {user.email}</h2>
         <LogoutButton />
       </div>
-      <h2>Upload image</h2>
-      <UploadImage />
 
-      <h1>Menu Management</h1>
-      <EditMenu owner_id={user.id} />
+      <h1 className="my-8">Beheer Menu</h1>
+
+      <div className="my-8 flex flex-col gap-8">
+        
+        <EditMenu owner_id={user.id} />
+      </div>
     </div>
   );
 }
---
+++ apps/frontend/src/components/design/menu-items/MenuItemBackOffice.tsx
@@ -0,0 +1,40 @@
+import { Pencil, Trash } from "lucide-react";
+import { MenuItem } from "@/modules/menu/types";
+
+interface MenuItemBackOfficeProps {
+  item: MenuItem;
+  onEdit: (item: MenuItem) => void;
+  onDelete: (id: number) => void;
+}
+
+export default function MenuItemBackOffice({
+  item,
+  onEdit,
+  onDelete,
+}: MenuItemBackOfficeProps) {
+  return (
+    <div className="flex justify-between items-center p-4 gap-4 border border-primary200 rounded-lg shadow-md">
+      <div className="w-full">
+        <div className="flex items-center justify-between gap-4">
+          <h2 className="text-lg font-semibold">{item.name}</h2>
+          <p className="text-sm font-bold min-w-10">€ {item.price}</p>
+        </div>
+        <p className="text-gray-600 text-sm italic">
+          {Array.isArray(item.ingredients) && item.ingredients.join(" - ")}
+        </p>
+      </div>
+      <button
+        onClick={() => onDelete(item.id)}
+        className="p-2 rounded-lg hover:bg-gray-100"
+      >
+        <Trash className="w-5 h-5 text-blue-500" />
+      </button>
+      <button
+        onClick={() => onEdit(item)}
+        className="p-2 rounded-lg hover:bg-gray-100"
+      >
+        <Pencil className="w-5 h-5 text-blue-500" />
+      </button>
+    </div>
+  );
+}
