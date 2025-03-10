0099447fedc9479bcd8e0656aaa2f8c3f9275f01
pgm-arnoboenders <arnoboen@student.arteveldehs.be>
2025-03-10 13:29:03 +0100

    update styling

--- apps/frontend/src/app/(website)/order/page.tsx
+++ apps/frontend/src/app/(website)/order/page.tsx
@@ -5,12 +5,12 @@ export default function OrderPage() {
   return (
     <div className="container my-32">
       <h1>Bestel</h1>
-      <div className="flex justify-between gap-8">
+      <div className="flex flex-col lg:flex-row-reverse justify-between gap-8">
+        <div className="w-1/3">
+          <OrderBox layout="sticky" />
+        </div>
         <div className="w-2/3">
           <OrderMenu />
-        </div>
-        <div className="w-1/3">
-          <OrderBox layout="sticky"/>
         </div>
       </div>
     </div>
