64dab33580a9bea3b4d6c15822ef6b84d809e54d
pgm-arnoboenders <arnoboen@student.arteveldehs.be>
2025-03-12 13:43:55 +0100

    Fix for price and some styling

--- apps/frontend/src/app/(website)/order/page.tsx
+++ apps/frontend/src/app/(website)/order/page.tsx
@@ -6,10 +6,10 @@ export default function OrderPage() {
     <div className="container my-32">
       <h1>Bestel</h1>
       <div className="flex flex-col lg:flex-row-reverse justify-between gap-8">
-        <div className="w-1/3">
+        <div className="lg:w-1/3">
           <OrderBox layout="sticky" />
         </div>
-        <div className="w-2/3">
+        <div className="lg:w-2/3">
           <OrderMenu />
         </div>
       </div>
