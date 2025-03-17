b7b32ce693a90fcbe844adf266e318f00fc76fbc
pgm-arnoboenders <arnoboen@student.arteveldehs.be>
2025-03-17 16:19:56 +0100

    Fix for time id instead of time

---
+++ apps/frontend/src/modules/time-slots/api.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@/core/networking/api";
+import { TimeSlot } from "./types";
+
+export const getTime = async ({ id }: { id: number }): Promise<TimeSlot[] | null> => {
+  const { data, error } = await supabase
+    .from("takeaway_time_slots")
+    .select("*")
+    .eq("id", id);
+  if (error) {
+    throw error;
+  }
+  return Promise.resolve(data);
+};
