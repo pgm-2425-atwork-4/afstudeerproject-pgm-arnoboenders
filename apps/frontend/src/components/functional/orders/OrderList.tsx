0f72cb845b51918bf5b95c7a3be8af5aa634caa3
pgm-arnoboenders <arnoboen@student.arteveldehs.be>
2025-03-20 14:57:53 +0100

    Fix for sorting

--- apps/frontend/src/components/functional/orders/OrderList.tsx
+++ apps/frontend/src/components/functional/orders/OrderList.tsx
@@ -9,7 +9,9 @@ export default function OrderList() {
       <div className="grid grid-cols-11 gap-4 items-center p-4 rounded-xl">
         <p className="flex justify-center font-bold">Afgehaald</p>
         <p className="flex justify-center font-bold">Naam</p>
-        <p className="flex justify-center font-bold col-span-2">Telefoonnummer</p>
+        <p className="flex justify-center font-bold col-span-2">
+          Telefoonnummer
+        </p>
         <p className="flex justify-center font-bold">Afhaaltijd</p>
         <p className="flex justify-center font-bold">Prijs</p>
         <p className="col-span-3 flex justify-center font-bold">Bestelling</p>
@@ -17,11 +19,21 @@ export default function OrderList() {
       </div>
       <ul>
         {orders &&
-          orders.map((order) => (
-            <li key={order.id} className="my-4">
-              <OrderCard order={order} />
-            </li>
-          ))}
+          orders
+            .sort((a, b) => {
+              if (a.picked_up === b.picked_up) {
+                return (
+                  new Date(a.take_away_time).getTime() -
+                  new Date(b.take_away_time).getTime()
+                );
+              }
+              return a.picked_up ? 1 : -1;
+            })
+            .map((order) => (
+              <li key={order.id} className="my-4">
+                <OrderCard order={order} />
+              </li>
+            ))}
       </ul>
     </div>
   );
