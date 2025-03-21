806fc7e6989b7bce1dd8b4099c7f450b43550ece
pgm-arnoboenders <arnoboen@student.arteveldehs.be>
2025-03-21 14:01:23 +0100

    Update styling

--- apps/frontend/src/components/design/menu-items/MenuItemCard.tsx
+++ apps/frontend/src/components/design/menu-items/MenuItemCard.tsx
@@ -5,7 +5,7 @@ export default function MenuItemCard({ item }: { item: MenuItem }) {
   return (
     <div className="flex flex-col rounded-xl py-4">
       <div className="flex justify-between items-center">
-        <h3 className="flex items-start gap-2">
+        <h3 className="flex items-start gap-2 mb-0">
           {item.veggie && <Vegan className="text-green-600" />}
           {item.name}
         </h3>
